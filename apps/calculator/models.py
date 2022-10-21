fb3fbf7574194f1dd267fe7aea63db2a391ecdc8
fernandomelendez25 <00018720@uca.edu.sv>
2022-10-21 16:16:55 -0600

    🗃️(model: Diagnostico) Set to descendent ordering

--- apps/calculator/models.py
+++ apps/calculator/models.py
@@ -1,32 +1,33 @@
 from random import choices
 from django.db import models
-import datetime
 
 # Modelo de diagnostico de un usuario
 # TODO: Falta realizar la relacion entre un user y el diagnostico
+
 class Diagnostico(models.Model):
     LOW_WEIGHT = 'Bajo peso'
     NORMAL_WEIGHT = 'Peso normal'
     OVERWEIGHT = 'Sobrepeso'
     OBESITY = 'Obesidad'
-            
+
     estadoPeso = [
         (LOW_WEIGHT, 'Bajo peso'),
         (NORMAL_WEIGHT, 'Peso normal'),
         (OVERWEIGHT, 'Sobrepeso'),
         (OBESITY, 'Obesidad'),
     ]
-            
+
     fecha_diagnostico = models.DateField(auto_now_add=True, blank=True)
     estatura = models.FloatField(blank=False, null=False)
     peso = models.FloatField(blank=False, null=False)
-    imc = models.FloatField(blank = True, null = False)
-    estado = models.CharField(max_length = 15, choices=estadoPeso, blank=True, null=False)
-    
+    imc = models.FloatField(blank=True, null=False)
+    estado = models.CharField(
+        max_length=15, choices=estadoPeso, blank=True, null=False)
+
     # Funcion para calcular el imc
     def calc_imc(self):
         self.imc = self.peso / ((self.estatura)**2)
-    
+
     # Funcion para comparar el IMC y asignar peso
     def compare_imc(self):
         if 0 <= self.imc < 18.5:
@@ -37,17 +38,17 @@ class Diagnostico(models.Model):
             self.estado = self.OVERWEIGHT
         else:
             self.estado = self.OBESITY
-            
+
     def save(self, *args, **kwargs):
 
         # Se calcula el imc
         self.calc_imc()
-        
+
         # Comparacion de pesos para asignar uno
         self.compare_imc()
-        
+
         # Guardar nuevos datos en BD
         super().save(*args, **kwargs)
-        
+
     class Meta:
-        ordering = ['id']
+        ordering = ['-id']
